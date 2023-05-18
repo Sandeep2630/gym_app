64ec26098352a0e78167f2a978550d9d7ec287cd
sandeep2630 <katwal.sandeep28@gmail.com>
2023-05-18 20:25:07 +0530

    added login and sign up routes now can navigate between two pages

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function Login() {
+  const navigate= useNavigate()
   return (
 
 <section className="bg-gray-50 dark:bg-gray-900">
@@ -31,8 +33,9 @@ export default function Login() {
                       <a href="#" className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
                   </div>
                   <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
-                  <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                      Don’t have an account yet? <a href="#" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
+                  <p  
+                   className="text-sm font-light text-gray-500 dark:text-gray-400">
+                      Don’t have an account yet? <Link to='/register' className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</Link>
                   </p>
               </form>
           </div>
--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Register() {
   return ( 
@@ -34,7 +35,7 @@ export default function Register() {
                   </div>
                   <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Create an account</button>
                   <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                      Already have an account? <a href="#" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Login here</a>
+                      Already have an account? <Link to='/login' className="font-medium text-primary-600 hover:underline dark:text-primary-500">Login here</Link>
                   </p>
               </form>
           </div>
